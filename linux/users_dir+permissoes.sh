2b7b39f18b9a3f9247341845bf6a090745103f0b
Mogleson Lima <moglesonlima@gmail.com>
2022-10-26 17:36:55 -0300

    doc(./*): Commmit inicial, atividade 01.
    Script -> Criação de users, diretórios e permições.

---
+++ linux/users_dir+permissoes.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "Criando diretórios..."
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando GRP de Users..."
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários..."
+# Definindo ->  Bash e senha e GRP(poderia usado o usermod)
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt senha123) -g GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt senha123) -g GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt senha123) -g GRP_ADM
+
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt senha123) -g GRP_ADM
+useradd sebastinan -m -s /bin/bash -p $(openssl passwd -crypt senha123)
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt senha123)
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt senha123)
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt senha123)
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt senha123)
+
+echo "Definindo permições dos diretórios..."
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "The And..."
